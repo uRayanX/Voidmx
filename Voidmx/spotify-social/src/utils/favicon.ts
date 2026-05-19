b802b62e02eee37c59bdeb6f61df19a3470b4cf4
tonni2 <tannislam42@gmail.com>
2026-05-19 04:06:41 -0700

    Commit changes and ignore .DS_Store

--- Voidmx/spotify-social/src/utils/favicon.ts
+++
@@ -1,9 +0,0 @@
-import type { TidalTrack } from '../types/tidal';
-
-export function updateFaviconAndTitle(track: TidalTrack | null) {
-  if (!track) {
-    document.title = 'Void';
-    return;
-  }
-  document.title = `${track.title} - ${track.artists[0]?.name || 'Unknown Artist'}`;
-}
